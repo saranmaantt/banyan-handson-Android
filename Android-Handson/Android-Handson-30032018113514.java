6a6ed3df94f058b9179a4ef4d3b69679cb344858
saranmaantt <37173664+saranmaantt@users.noreply.github.com>
2018-03-30 11:35:18 +0530

    File creation

---
+++ Android-Handson/Android-Handson-30032018113514.java
@@ -0,0 +1,35 @@
+/* IMPORTANT: Use Class Name as Main. */
+
+/*
+ * uncomment this if you want to read input.
+//imports for BufferedReader
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//import for Scanner and other utility  classes
+import java.util.*;
+*/
+
+class Main {
+    public static void main(String args[] ) throws Exception {
+        /*
+         * Read input from stdin and provide input before running
+         * Use either of these methods for input
+
+        //BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        int N = Integer.parseInt(line);
+
+        //Scanner
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            System.out.println("hello world");
+        }
+        */
+
+        System.out.println("Hello World!");
+    }
+}
